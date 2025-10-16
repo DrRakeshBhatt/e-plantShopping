ec3aad8dad79f4787f27250063e9674448d104e9
DrRakeshBhatt <rakeshbhatt95597@gmail.com>
2025-10-16 05:49:57 -0400

    second update

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -5,31 +5,48 @@ import './CartItem.css';
 
 const CartItem = ({ onContinueShopping }) => {
   const cart = useSelector(state => state.cart.items);
+  const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
   const dispatch = useDispatch();
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+    let total = 0;
+    
+    cart.forEach((item) => {
+        total += parseFloat(item.cost.substring(1)) * item.quantity;
+    });
+
+    return total;
   };
 
   const handleContinueShopping = (e) => {
-   
+    e.preventDefault();
+    setShowPlants(true); // Set showAboutUs to true when "About Us" link is clicked
+    setShowCart(false);
   };
 
-
-
   const handleIncrement = (item) => {
+    dispatch(updateQuantity({ name: item.name, quantity: item.quantity + 1 }));
   };
 
   const handleDecrement = (item) => {
-   
+    if (item.quantity > 1) {
+        dispatch(updateQuantity({ name: item.name, quantity: item.quantity - 1 }));
+    }
+    else{
+     dispatch(removeItem(item.name));
+    }
+    
   };
 
   const handleRemove = (item) => {
+    dispatch(removeItem(item.name));
   };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+    total = parseFloat(item.cost.substring(1)) * item.quantity;
+    return total;
   };
 
   const handleCheckoutShopping = (e) => {
